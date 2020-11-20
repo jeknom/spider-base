15cc381bace7ea68e1767c57ce3235e5bfabfcd4
Johannes Palvanen <johannes.palvanen@seriously.com>
2020-11-21 00:04:57 +0200

    Clean up robots txt validator with a simple regular expression

--- src/RobotsTxtValidator.ts
+++ src/RobotsTxtValidator.ts
@@ -1,31 +1,36 @@
-import { URL, URLSearchParams } from "url";
+import { URL } from "url";
 import Logger from "./utils/Logger";
-
-type userAgent = {
-  name: string,
-  paths: accessPath[]
-}
-
-type accessPath = {
-  isAllowed: boolean,
-  path: string
-}
+import axios from 'axios';
 
 export default class RobotsTxtValidator {
-  agents: userAgent[] = [];
-  logger: Logger = new Logger(RobotsTxtValidator.name);
+  private robotsTxt?: string;
+  private hasAttemptedToFetchRobotsTxt: boolean = false;
+  private logger: Logger = new Logger(RobotsTxtValidator.name);
 
-  isPathAllowed(fullUrl: string): boolean {
-    const url = new URL(fullUrl);
-    for (let agent of this.agents)
-    for (let path of agent.paths) {
-      const validQuery = this.isValidQueryParams(url.searchParams, path);
-      if (!validQuery) {
-        return false;
+  async isUrlAllowed(url: URL): Promise<boolean> {
+    if (!this.hasAttemptedToFetchRobotsTxt) {
+      const robotsTxtUrl = `${url.origin}/robots.txt`;
+      
+      try {
+        this.hasAttemptedToFetchRobotsTxt = true;
+        const response = await axios.get(robotsTxtUrl);
+        this.robotsTxt = response.data;
+        this.logger.log(`Found robots.txt from ${robotsTxtUrl}.`);
+      } catch (error) {
+        this.logger.logWarning(`Could not fetch robots.txt from ${robotsTxtUrl}.`);
       }
+    }
+    
+    if (this.robotsTxt && !this.isAllowedPath(url.pathname)) {
+      return false;
+    }
 
-      const validPath = this.isValidPath(url.pathname, path);
-      if (!validPath) {
+    return true;
+  }
+
+  private isAllowedPath(pathName: string): boolean {
+    for (let pathPart of pathName.split('/').filter(n => n !== '')) {
+      if (!this.isAllowed(pathPart)) {
         return false;
       }
     }
@@ -33,44 +38,10 @@ export default class RobotsTxtValidator {
     return true;
   }
 
-  private isValidQueryParams(searchParams: URLSearchParams, robotsTxtPath: accessPath): boolean {
-    for (let queryParam of searchParams.entries()) {
-      if (!this.isValid(`${queryParam[0]}=`, robotsTxtPath)) {
-        return false;
-      }
-    }
-    return true;
-  }
+  private isAllowed(value: string): boolean {
+    const expression = new RegExp(`[Dd]isallow: (\\/|\\*\\/|\\/\\*|\\/\\*\\/)${value}(\\$|$|\\/|\\/\\*|\\/\\*\\?)`);
+    const matches = this.robotsTxt?.match(expression) || [];
 
-  private isValidPath(pathName: string, robotsTxtPath: accessPath): boolean {
-    for (let pathPart of pathName.split('/')) {
-      if (!this.isValid(pathPart, robotsTxtPath)) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  private isValid(urlPath: string, robotsTxtPath: accessPath): boolean {
-    const { isAllowed, path } = robotsTxtPath;
-
-    if (!isAllowed) {
-      const isNotBlocking = 
-        path !== '*' &&  
-        urlPath !== `/${path}/` && 
-        urlPath !== `${path}*` &&
-        urlPath !== `${path}/*` && 
-        urlPath !== `${path}*/` && 
-        urlPath !== `${path}/*/` &&
-        urlPath !== `*${path}` &&
-        urlPath !== `*/${path}` && 
-        urlPath !== `/*${path}` &&
-        urlPath !== `/*/${path}`;
-  
-      return isNotBlocking;
-    }
-
-    return true;
+    return matches.length > 0;
   }
 }
